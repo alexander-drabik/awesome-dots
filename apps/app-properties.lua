6c1d2321925ccfa4b7591f6ccb24fec8ce58711b
alexander-drabik <xelada91@o2.pl>
2024-07-20 18:47:28 +0200

    Last state of dots before moving to hyprland

--- apps/app-properties.lua
+++ apps/app-properties.lua
@@ -1,38 +1,45 @@
 local rules = {
-    -- All clients will match this rule.
-    { 
-		rule = { },
-		properties = { 
+	-- All clients will match this rule.
+	{
+		rule = {},
+		properties = {
 			border_width = beautiful.border_width,
-		    border_color = beautiful.border_normal,
-	        focus = awful.client.focus.filter,
-            raise = true,
+			border_color = beautiful.border_normal,
+			focus = awful.client.focus.filter,
+			raise = true,
 			keys = clientkeys,
 			buttons = clientbuttons,
 			maximized = false,
 			maximized_horizontal = false,
 			maximized_vertical = false,
 			screen = awful.screen.preferred,
-			placement = awful.placement.no_overlap+awful.placement.no_offscreen
+			placement = awful.placement.no_overlap + awful.placement.no_offscreen
 		}
-    },
+	},
 
-    -- Floating clients.
-    {
+	-- Floating clients.
+	{
 		rule_any = require "apps.floating-clients",
 		properties = { floating = true }
 	},
 
 	-- App properties
-    {
+	{
 		rule = { class = "firefox" },
-		properties = { tag = "2" } 
+		properties = { tag = "2" }
 	},
-    {
+	{
+		rule = { class = "Vivaldi-stable" },
+		properties = { tag = "2" }
+	},
+	{
 		rule = { class = "discord" },
-		properties = { tag = "1" } 
+		properties = { tag = "1" }
+	},
+	{
+		rule = { class = "agent.py" },
+		properties = { tag = "9" }
 	},
 }
 
 return rules
-
